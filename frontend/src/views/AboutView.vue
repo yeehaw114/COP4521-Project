e14f451a4dbc60841d9fb007ab19dfac42c27af4
Alex Gonzalez <galex2003@icloud.com>
2024-06-05 12:23:10 -0400

    Made about view a card

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <h3 class="text-h3">About Us</h3>
-    <h6 class="text-h6">
-      Contributors: Alex Gonzalez, Zachary Lima, Gavin Mcdavitt, Ethan Burke, Zachary Herman,
-      Fernando de Torres
-    </h6>
-    <p class="text-body-1">This is a project made for COP 4521 at Florida State University.</p>
-  </div>
+  <v-card class="mx-auto pa-6" elevation="15" max-width="500" rounded="md">
+    <div>
+      <div align="center" class="mx-auto text-h3">About Us</div>
+      <v-divider :thickness="5"></v-divider>
+      <div class="text-h6">
+        Contributors: Alex Gonzalez, Zachary Lima, Gavin Mcdavitt, Ethan Burke, Zachary Herman,
+        Fernando de Torres
+      </div>
+      <div class="text-body-1">
+        This is a project made for COP 4521 at Florida State University.
+      </div>
+    </div>
+  </v-card>
 </template>
 
 <script setup lang="ts"></script>
