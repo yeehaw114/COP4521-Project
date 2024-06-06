99913c8b60baf80891dce163315b9bb113df51c0
Alex Gonzalez <galex2003@icloud.com>
2024-06-05 23:10:06 -0400

    Added models that map directly to sql tables

--- backend/workout/models.py
+++ backend/workout/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 
-# Create your models here.
+# Tables
 class User(models.Model):
     name = models.CharField(max_length=30)
     password = models.CharField(max_length=60)
     description = models.CharField(max_length=500)
+    join_date = models.TimeField()
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=30)
+    muscle_group = models.CharField(max_length=30)
+
+class Workout_Exercises(models.Model):
+    workout_id = models.CharField(max_length=10)
+    exercise_name = models.CharField(max_length=30)
+    reps = models.SmallIntegerField()
+    weight = models.SmallIntegerField()
+
+class Workout(models.Model):
+    id = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=30)
+
+class User_Workouts(models.Model):
+    id = models.CharField(max_length=10)
+    workout_id = models.CharField(max_length=10)
+    username = models.CharField(max_length=30)
+    done_date = models.TimeField()
+
+class User_Sets(models.Model):
+    id = models.CharField(max_length=10)
+    user_workout_id = models.CharField(max_length=10)
+    reps = models.SmallIntegerField()
+    weight = models.SmallIntegerField()
+    username = models.CharField(max_length=30)
