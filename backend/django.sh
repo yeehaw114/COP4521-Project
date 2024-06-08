1b46ab09fa173377d2a76b3d4f6869c400db2bac
zlima <endercraft68@gmail.com>
2024-06-07 22:03:25 -0400

    chore: Add migrations to django.sh to apply changes

--- backend/django.sh
+++ backend/django.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Making migrations..."
+python3 manage.py migrate
+echo ======================
+
 echo "Starting Server..."
 python3 manage.py runserver 0.0.0.0:8080
 echo ===================================
