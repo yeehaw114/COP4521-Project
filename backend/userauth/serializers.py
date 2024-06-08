67de25bd6fb92b856be2fed52f33232bc5d0d1b5
zlima <endercraft68@gmail.com>
2024-06-08 17:16:17 -0400

    Refactor serializers.py to fix error

--- backend/userauth/serializers.py
+++ backend/userauth/serializers.py
@@ -21,8 +21,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            firstname=validated_data['first_name'],
-            lastname=validated_data['last_name'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
