2e2a4cd41fb8c4f120e7ea3dcae452832489aecb
zlima <endercraft68@gmail.com>
2024-06-07 21:31:18 -0400

    Create profiles view and register it

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'workout',
-    'rest_framework'
+    'rest_framework',
+    'profiles.apps.ProfilesConfig',
 ]
 
 MIDDLEWARE = [
