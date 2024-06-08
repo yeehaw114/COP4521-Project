7b22c27f9d236a7328b5bdf52bcf84736ffca750
zlima <endercraft68@gmail.com>
2024-06-07 20:53:28 -0400

    Create Profile model and register it in profiles/admin.py

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class Profile(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
