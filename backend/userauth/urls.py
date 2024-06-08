02e8250785901106d5d64c9409cb085121b269c8
zlima <endercraft68@gmail.com>
2024-06-08 17:01:38 -0400

    Refactor routes

--- backend/userauth/urls.py
+++ backend/userauth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import login_view, logout_view, register_view
 
 urlpatterns = [
-    path('api/login/', login_view),
-    path('api/logout/', logout_view),
-    path('api/register/', register_view),
+    path('api/login', login_view),
+    path('api/logout', logout_view),
+    path('api/register', register_view),
 ]
