96f423cfa56c5c5e2b762ef56612183316032279
Alex Gonzalez <galex2003@icloud.com>
2024-06-10 19:30:05 -0400

    working on csrf tokens

--- frontend/src/requests/auth.ts
+++ frontend/src/requests/auth.ts
@@ -1,5 +1,24 @@
 import type { LoginCreds, RegisterCreds } from '@/types/credentials'
 import { SERV_NAME } from '@/ts/host'
+
+export async function getToken() {
+  await fetch(SERV_NAME + '/api/csrf-token', {
+    method: 'GET'
+  })
+  .then((response) => {
+    if (!response.ok) {
+     throw new Error(response.statusText) 
+    }
+  })
+  .then((data)=> {
+    console.log(data)
+    //localStorage.setItem('csrf-token',data)
+  })
+  .catch((error)=>{
+    console.error(`App may not work as expected: ${error}` )
+  })
+}
+
 export async function postSignup(creds: RegisterCreds) {
   await fetch(SERV_NAME + '/api/register', {
     method: 'POST',
