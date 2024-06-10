7e0dc65f124d2951f71aa3e76a2cf25e2e216a8f
zlima <endercraft68@gmail.com>
2024-06-10 19:28:08 -0400

    Comment out unused urls in workout app to run docker

--- backend/workout/urls.py
+++ backend/workout/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import WorkoutsViewSet, SetsViewSet, UserWorkoutsViewSet, UserSetsViewSet
+from .views import WorkoutsViewSet
 
 router = DefaultRouter()
 router.register(r'workouts', WorkoutsViewSet)
-router.register(r'sets', SetsViewSet)
-router.register(r'user_workouts', UserWorkoutsViewSet)
-router.register(r'user_sets', UserSetsViewSet)
+#router.register(r'sets', SetsViewSet)
+#router.register(r'user_workouts', UserWorkoutsViewSet)
+#router.register(r'user_sets', UserSetsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
