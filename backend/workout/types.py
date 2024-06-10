47c36926acf68378c1fe13dbfa9ec0406bbb46f3
Alex Gonzalez <galex2003@icloud.com>
2024-06-10 18:30:12 -0400

    updated types

--- backend/workout/types.py
+++ backend/workout/types.py
@@ -1,13 +1,16 @@
-
+from typing import List
 
 class Set:
-    reps:int
-    weight:int
+    def __init__(self, reps:int, weight:int):
+        self.reps=reps
+        self.weight=weight
 
 class Exercise:
-    name:str
-    sets: [Set]
+    def __init__(self, name:str, sets:List[Set]):
+        self.name=name
+        self.sets=sets
 
 class Workout:
-    name:str
-    exercises: [Exercise]
+    def __init__(self, name:str, exercises:List[Exercise]):
+        self.name = name
+        self.exercises = exercises
