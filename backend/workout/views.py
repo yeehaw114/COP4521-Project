38adcde09e35c0ad74a3d6bb5bb5f6bfdcf4d4d5
zlima <endercraft68@gmail.com>
2024-06-09 20:38:54 -0400

    Finish workout creation view to include sessions

--- backend/workout/views.py
+++ backend/workout/views.py
@@ -5,3 +5,17 @@ from rest_framework.response import Response
 from .models import Workout
 from .serializers import WorkoutSerializer
 from django.contrib.auth.decorators import login_required
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@login_required
+def create_workout(request):
+    user = request.user
+    data = request.data.copy()
+    data['username'] = user.username
+    serializer = WorkoutSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
