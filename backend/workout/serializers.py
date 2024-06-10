b59bd4c468ad83139555233355852768787f0e76
zlima <endercraft68@gmail.com>
2024-06-09 20:32:25 -0400

    Define serializers for workout template backend

---
+++ backend/workout/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Workout, Workout_Exercises, Exercise
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ['name', 'muscle_group']
+
+class WorkoutExercisesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout_Exercises
+        fields = ['exercise_name', 'reps', 'weight']
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    exercises = WorkoutExercisesSerializer(many=True)
+    
+    class Meta:
+        model = Workout
+        fields = ['id', 'name', 'exercises']
+        depth = 1
+    def create(self, validated_data):
+        exercises_data = validated_data.pop('exercises')
+        workout = Workout.objects.create(**validated_data)
+        for exercise_data in exercises_data:
+            Workout_Exercises.objects.create(workout_id=workout.id, **exercise_data)
+        return workout
