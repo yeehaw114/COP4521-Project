b33ca596e9133b4ffc9129ceb17b946ef8618758
zlima <endercraft68@gmail.com>
2024-06-10 19:15:01 -0400

    Finalize new serializers

--- backend/workout/serializers.py
+++ backend/workout/serializers.py
@@ -1,17 +1,13 @@
 from rest_framework import serializers
-from .models import Workouts, Workout_Exercises, Exercise, User_Workouts, User_Sets
+from .models import Workouts, Sets, User_Workouts, User_Sets
 
-class WorkoutSerializer(serializers.ModelSerializer):
+class WorkoutsSerializer(serializers.ModelSerializer):
     model = Workouts
     fields = ['id', 'name', 'username']
 
-class ExerciseSerializer(serializers.ModelSerializer):
-    model = Exercise
-    fields = ['id', 'name', 'muscle_group']
-
-class WorkoutExercisesSerializer(serializers.ModelSerializer):
-    model = Workout_Exercises
-    fields = ['id', 'workout_id', 'exercise_name', 'reps', 'weight']
+class SetsSerializer(serializers.ModelSerializer):
+    model = Sets
+    fields = ['id', 'workout_id', 'exercise', 'reps', 'weight']
 
 class UserWorkoutsSerializer(serializers.ModelSerializer):
     model = User_Workouts
@@ -19,4 +15,4 @@ class UserWorkoutsSerializer(serializers.ModelSerializer):
 
 class UserSetsSerializer(serializers.ModelSerializer):
     model = User_Sets
-    fields = ['id', 'user_workout_id', 'reps', 'weight', 'username']
+    fields = ['id', 'user_workout_id', 'set_id', 'reps', 'weight', 'username']
--- backend/workout/views.py
+++ backend/workout/views.py
@@ -3,34 +3,11 @@ from rest_framework import status
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import Workout, Exercise, Workout_Exercises, User_Workouts, User_Sets
-from .serializers import WorkoutSerializer, ExerciseSerializer, WorkoutExercisesSerializer, UserWorkoutsSerializer, UserSetsSerializer
+from .models import Workouts, Sets, User_Workouts, User_Sets
+from .serializers import WorkoutsSerializer, SetsSerializer, UserWorkoutsSerializer, UserSetsSerializer
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import login_required
 
 @permission_classes([IsAuthenticated])
 @csrf_protect
 @login_required
-class WorkoutViewSet(viewsets.ModelViewSet):
-    queryset = Workout.objects.all()
-    serializer_class = WorkoutSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(username=self.request.user)
-
-@permission_classes([IsAuthenticated])
-@csrf_protect
-@login_required
-class ExerciseViewSet(viewsets.ModelViewSet):
-    queryset = Exercise.objects.all()
-    serializer_class = ExerciseSerializer
-
-@permission_classes([IsAuthenticated])
-@csrf_protect
-@login_required
-class WorkoutExercisesViewSet(viewsets.ModelViewSet):
-    queryset = Workout_Exercises.objects.all()
-    serializer_class = WorkoutExercisesSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(workout_id=self.request.data['workout_id'], exercise_name=self.request.data['exercise_name'])
