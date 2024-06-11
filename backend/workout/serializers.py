89d03600229997629e9569138dc45387c9109b2f
zlima <endercraft68@gmail.com>
2024-06-11 13:46:23 -0400

    Final serializer iteration, should work for what we are trying to do

--- backend/workout/serializers.py
+++ backend/workout/serializers.py
@@ -2,36 +2,38 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Workouts, Sets, User_Workouts, User_Sets
 
-class SetSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Sets
-        fields = ['exercise', 'reps', 'weight']
+        model = User
+        fields = ['id', 'username']
 
-class WorkoutSerializer(serializers.ModelSerializer):
-    sets = SetSerializer(many=True)
+class WorkoutsSerializer(serializers.ModelSerializer):
+    username = UserSerializer(read_only=True)
 
     class Meta:
         model = Workouts
-        fields = ['id', 'name', 'sets']
-        read_only_fields = ['id']
+        fields = ['id', 'name', 'username']
 
-    def create(self, validated_data):
-        sets_data = validated_data.pop('sets')
-        workout = Workouts.objects.create(**validated_data)
-        for set_data in sets_data:
-            Sets.objects.create(workout_id=workout, **set_data)
-        return workout
+class SetsSerializer(serializers.ModelSerializer):
+    workout_id = WorkoutsSerializer(read_only=True)
 
-class UserWorkoutSerializer(serializers.ModelSerializer):
-    sets = SetSerializer(source='workout_id.sets_set', many=True)
+    class Meta:
+        model = Sets
+        fields = ['id', 'workout_id', 'exercise', 'reps', 'weight']
+
+class UserWorkoutsSerializer(serializers.ModelSerializer):
+    workout_id = WorkoutsSerializer(read_only=True)
+    username = UserSerializer(read_only=True)
 
     class Meta:
         model = User_Workouts
-        fields = ['id', 'username', 'workout_id', 'done_date', 'sets']
-        read_only_fields = ['id', 'username']
+        fields = ['id', 'workout_id', 'username', 'done_date']
 
-class UserSetSerializer(serializers.ModelSerializer):
+class UserSetsSerializer(serializers.ModelSerializer):
+    user_workout_id = UserWorkoutsSerializer(read_only=True)
+    set_id = SetsSerializer(read_only=True)
+    username = UserSerializer(read_only=True)
+
     class Meta:
         model = User_Sets
         fields = ['id', 'user_workout_id', 'set_id', 'reps', 'weight', 'username']
-        read_only_fields = ['id', 'username']
