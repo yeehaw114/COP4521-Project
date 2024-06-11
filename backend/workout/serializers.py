b2725e37db1349e8a74266b54fb1516f999180d9
zlima <endercraft68@gmail.com>
2024-06-10 20:24:09 -0400

    Fix serializers and models

--- backend/workout/serializers.py
+++ backend/workout/serializers.py
@@ -13,6 +13,7 @@ class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workouts
         fields = ['id', 'name', 'sets']
+        read_only_fields = ['id']
 
     def create(self, validated_data):
         sets_data = validated_data.pop('sets')
@@ -22,13 +23,15 @@ class WorkoutSerializer(serializers.ModelSerializer):
         return workout
 
 class UserWorkoutSerializer(serializers.ModelSerializer):
-    sets = SetSerializer(source='workout_id.sets_set', many=True)  # Access through related_name if set, or default related manager
+    sets = SetSerializer(source='workout_id.sets_set', many=True)
 
     class Meta:
         model = User_Workouts
         fields = ['id', 'username', 'workout_id', 'done_date', 'sets']
+        read_only_fields = ['id', 'username']
 
 class UserSetSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_Sets
         fields = ['id', 'user_workout_id', 'set_id', 'reps', 'weight', 'username']
+        read_only_fields = ['id', 'username']
