8a40ddca5b5874184d082c50ede9c15a8f4a910f
Zachary Herman <zherman114@gmail.com>
2024-06-10 23:21:16 -0400

    Made a vue.js component for displaying templates

---
+++ frontend/src/components/DisplayTemplate.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container class="bg-surface-variant">
+      <v-row no-gutters>
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          cols="12"
+          sm="4"
+        >
+          <v-sheet class="ma-2 pa-2">
+            <v-card
+                class="mx-auto"
+                href=""
+                max-width="344"
+                height="100"
+                prepend-icon="mdi-dumbbell"
+                rel="noopener"
+                target="_blank"
+                title="WORKOUT_NAME_GOES_HERE"
+            ></v-card>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+
+</script>
