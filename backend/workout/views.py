fb793744779318ca45682094dd81fddaaf2ff793
Alex Gonzalez <galex2003@icloud.com>
2024-06-12 14:15:13 -0400

    Added tokens to frontend

--- backend/workout/views.py
+++ backend/workout/views.py
@@ -1,21 +1,21 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Workouts, Sets, User_Workouts, User_Sets
-from .serializers import WorkoutSerializer, SetSerializer, UserWorkoutSerializer, UserSetSerializer
+from .serializers import WorkoutsSerializer, SetsSerializer, UserWorkoutsSerializer, UserSetsSerializer
 
 class WorkoutViewSet(viewsets.ModelViewSet):
     queryset = Workouts.objects.all()
-    serializer_class = WorkoutSerializer
+    serializer_class = WorkoutsSerializer
     permission_classes = [IsAuthenticated]
 
 class SetViewSet(viewsets.ModelViewSet):
     queryset = Sets.objects.all()
-    serializer_class = SetSerializer
+    serializer_class = SetsSerializer
     permission_classes = [IsAuthenticated]
 
 class UserWorkoutViewSet(viewsets.ModelViewSet):
     queryset = User_Workouts.objects.all()
-    serializer_class = UserWorkoutSerializer
+    serializer_class = UserWorkoutsSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
@@ -23,7 +23,7 @@ class UserWorkoutViewSet(viewsets.ModelViewSet):
 
 class UserSetViewSet(viewsets.ModelViewSet):
     queryset = User_Sets.objects.all()
-    serializer_class = UserSetSerializer
+    serializer_class = UserSetsSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
