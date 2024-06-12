8c489e6fe2b3d52f663a33a0ce20173d87ec8dbf
zlima <endercraft68@gmail.com>
2024-06-12 15:36:28 -0400

    Still trying to figure it out

--- backend/workout/serializers.py
+++ backend/workout/serializers.py
@@ -13,6 +13,7 @@ class WorkoutsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workouts
         fields = ['id', 'name', 'username']
+        read_only_fields = ['id', 'username']
 
 class SetsSerializer(serializers.ModelSerializer):
     workout_id = WorkoutsSerializer(read_only=True)
@@ -20,6 +21,7 @@ class SetsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sets
         fields = ['id', 'workout_id', 'exercise', 'reps', 'weight']
+        read_only_fields = ['id']
 
 class UserWorkoutsSerializer(serializers.ModelSerializer):
     workout_id = WorkoutsSerializer(read_only=True)
@@ -28,6 +30,7 @@ class UserWorkoutsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_Workouts
         fields = ['id', 'workout_id', 'username', 'done_date']
+        read_only_fields = ['id', 'username']
 
 class UserSetsSerializer(serializers.ModelSerializer):
     user_workout_id = UserWorkoutsSerializer(read_only=True)
@@ -37,3 +40,10 @@ class UserSetsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_Sets
         fields = ['id', 'user_workout_id', 'set_id', 'reps', 'weight', 'username']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['username'] = user
+        user_set = User_Sets.objects.create(username=user, **validated_data)
+        return user_set
--- backend/workout/urls.py
+++ backend/workout/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import WorkoutViewSet, SetViewSet, UserWorkoutViewSet, UserSetViewSet
+from .viewsets import WorkoutsViewSet, SetsViewSet, UserWorkoutsViewSet, UserSetsViewSet
 
 router = DefaultRouter()
-router.register(r'workouts', WorkoutViewSet)
-router.register(r'sets', SetViewSet)
-router.register(r'user_workouts', UserWorkoutViewSet)
-router.register(r'user_sets', UserSetViewSet)
+router.register(r'workouts', WorkoutsViewSet)
+router.register(r'sets', SetsViewSet)
+router.register(r'user_workouts', UserWorkoutsViewSet)
+router.register(r'user_sets', UserSetsViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
