525965346e657685847e9c3587d67ea77561218a
Alex Gonzalez <galex2003@icloud.com>
2024-06-13 10:45:50 -0400

    Started work on token login

--- frontend/src/requests/auth.ts
+++ frontend/src/requests/auth.ts
@@ -9,6 +9,14 @@ type authResponse = {
   user: User
 }
 
+type refreshRequest = {
+  refresh: string
+}
+
+type refreshResponse = {
+  access:string
+}
+
 export async function postSignup(creds: RegisterCreds) {
   await fetch(SERV_NAME + '/api/auth/register/', {
     method: 'POST',
@@ -25,6 +33,7 @@ export async function postSignup(creds: RegisterCreds) {
     })
     .then((data: authResponse) => {
       localStorage.setItem('jwt-token', data.access)
+      localStorage.setItem('refresh-token', data.refresh)
       const userStore = useUserStore()
       userStore.user = data.user
       userStore.isLoggedIn = true
@@ -48,8 +57,36 @@ export async function postLogin(creds: LoginCreds) {
     })
     .then((data: authResponse) => {
       localStorage.setItem('jwt-token', data.access)
+      localStorage.setItem('refresh-token', data.refresh)
       const userStore = useUserStore()
       userStore.user = data.user
       userStore.isLoggedIn = true
     })
+}
+
+export async function tokenLogin() {
+  if (!localStorage.getItem('refresh-token')) { return }
+  const token = localStorage.getItem('refresh-token')
+  const refresh:refreshRequest = {
+    refresh: token ?? ""
+  }
+  await fetch(SERV_NAME+'/api/auth/refresh/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(refresh)
+  })
+  .then((response) => {
+    if(!response.ok) {
+      throw new Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then((data: refreshResponse) => {
+    localStorage.setItem('jwt-token', data.access)
+    const userStore = useUserStore()
+    // userStore.user = data.user
+    userStore.isLoggedIn = true
+  })
 }
