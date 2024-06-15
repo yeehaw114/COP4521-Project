aad6da95a60a8ee95e7bf9d3320fea1503623ed0
Alex Gonzalez <galex2003@icloud.com>
2024-06-15 16:13:32 -0400

    Added neccessary requirements for getting workout templates

--- frontend/src/requests/workout.ts
+++ frontend/src/requests/workout.ts
@@ -21,3 +21,22 @@ export async function postWorkout(workout: Workout) {
       return data
     })
 }
+
+export async function getWorkout(id: number) {
+  const token = localStorage.getItem('jwt-token')
+  await fetch(SERV_NAME + '/api/workouts/'+id+'/details/', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then((data:Workout) => {
+    return data
+  })
+}
--- frontend/src/types/workout.ts
+++ frontend/src/types/workout.ts
@@ -1,12 +1,14 @@
 export type Workout = {
   name: string
   sets: Set[]
+  id:number
 }
 
 export type Set = {
   exercise: string
   reps: number
   weight: number
+  id:number
 }
 
 export type Exercise = {
