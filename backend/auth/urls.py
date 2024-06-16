459e5a0bbf7808fceb0d565e1d2f711892525bd3
zlima410 <endercraft68@gmail.com>
2024-06-16 14:12:57 -0400

    Change login with email to login with username

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,9 +1,15 @@
 from rest_framework.routers import SimpleRouter
+from django.urls import path, include
 
-from auth.views import LoginViewSet, RegistrationViewSet, RefreshViewSet
+from auth.views import LoginViewSet, RegistrationViewSet, RefreshViewSet, TokenViewSet
 
 auth_router = SimpleRouter()
 
 auth_router.register(r'auth/login', LoginViewSet, basename='auth-login')
 auth_router.register(r'auth/register', RegistrationViewSet, basename='auth-register')
 auth_router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+urlpatterns = [
+    path('', include(auth_router.urls)),
+    path('auth/login/token', TokenViewSet.as_view({'get': 'validate_token'}), name='token_validate'),
+]
