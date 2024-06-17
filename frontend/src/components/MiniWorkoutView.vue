86f65ea8621da684e44327d49a3bd251859d6f77
Alex Gonzalez <galex2003@icloud.com>
2024-06-16 21:42:50 -0400

    Updated components

---
+++ frontend/src/components/MiniWorkoutView.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container class="pa-4" style="max-width: 400px;">
+      <v-card>
+        <v-card-title class="d-flex justify-center">
+          <h2>{{props.workout.name}}</h2>
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="view">View</v-btn>
+          <v-btn color="secondary" @click="log">Log</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </template>
+
+<script setup lang="ts">
+import type { Workout } from '@/types/workout'
+
+const props = defineProps<{
+  workout:Workout
+}>()
+
+const view = () => {
+
+}
+
+const log = () => {
+
+}
+
+</script>
+
+<style scoped></style>
