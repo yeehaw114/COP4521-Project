72649dac1abe51ad6f80509b0a4566b73ceed03c
Alex Gonzalez <galex2003@icloud.com>
2024-06-17 14:35:24 -0400

    Refactored views to have a read and a create version of both logs and templates

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,38 +1,48 @@
-<template v-if="contentLoaded">
-
+<template>
+  <Error v-if="errorOccured" text="Could not load resources."/>
+  <div v-if="contentLoaded">
+  <div class="text-h4">My Workouts</div>
   <div v-for="w in userWorkouts">
-    <h1>Sample Templates</h1>
-    <MiniWorkout :workout="w"/>
+    <MiniWorkoutView :workout="w"/>
   </div>
-  <!-- <div>
-    <v-row>
-      <v-col>
-        <v-sheet height="200">
-          <v-calendar class="h-10 w-20" color="primary"></v-calendar>
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </div> -->
+  <div class="text-h6" v-if="userWorkouts.length==0">No workouts created yet.</div>
+  <div class="text-h4">My Logs</div>
+  <div v-for="w in userLogs">
+    <MiniLogView :log="w"/>
+  </div>
+  <div class="text-h6" v-if="userLogs.length==0">No workouts logged yet.</div>
+  <div>
+  </div>
+
+  </div>
 </template>
 
 <script setup lang="ts">
 // import displayTemplate from '@/components/DisplayTemplate.vue'
 import type { Ref } from 'vue'
 import { ref, onMounted } from 'vue'
-import type { Workout } from '@/types/workout'
-import MiniWorkout from '@/components/MiniWorkoutView.vue'
+import type { Workout, MiniWorkout, MiniLog } from '@/types/workout'
+import MiniWorkoutView from '@/components/MiniWorkoutView.vue'
+import MiniLogView from '@/components/MiniLogView.vue'
+import Error from '@/components/ErrorComponent.vue'
 import { VCalendar } from 'vuetify/labs/VCalendar'
 import { getUserWorkouts } from '@/requests/workout'
+import { getUserLogs } from '@/requests/log'
 
 const contentLoaded = ref(false)
-const userWorkouts:Ref<Workout[]> = ref([])
+const errorOccured = ref(false)
+const userWorkouts:Ref<MiniWorkout[]> = ref([])
+const userLogs:Ref<MiniLog[]> = ref([])
 
 onMounted(async() => {
   try {
+    userLogs.value = await getUserLogs()
     userWorkouts.value = await getUserWorkouts()
-    contentLoaded.value = true
   } catch(error) {
+    errorOccured.value = true
     console.error(error)
+  } finally {
+    contentLoaded.value = true
   }
 })
 
