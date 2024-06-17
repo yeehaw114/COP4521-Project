675e9f69554eb7d56cfa71b55b78a046b812aa93
Alex Gonzalez <galex2003@icloud.com>
2024-06-17 12:54:16 -0400

    Fixed homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,51 +1,41 @@
-<script setup lang="ts">
-import { ref } from 'vue';
+<template v-if="contentLoaded">
 
-// Define types for the data
-interface Workout {
-  id: number;
-  name: string;
-  description: string;
-  created_at: string;
-}
-
-// Create refs for data and loading state
-const data = ref<Workout[]>([]);
-const dataLoaded = ref(false);
-
-// Function to fetch data
-const getData = async (): Promise<void> => {
-  try {
-    const response = await fetch("http://localhost:8080/api/workouts/");
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const workouts: Workout[] = await response.json();  // Expecting the response to be an array of Workout objects
-    data.value = workouts;  // Assigning the fetched workouts to the reactive ref
-    dataLoaded.value = true;  // Setting dataLoaded to true indicating data has been loaded
-  } catch (error) {
-    console.warn(error.message);  // Logging any errors that occur during the fetch
-  }
-};
-
-getData();
-
-</script>
-
-<template>
-  <div>
-    <div v-if="!dataLoaded">Loading...</div>
-    <div v-else>
-      <ul>
-        <li v-for="workout in data" :key="workout.id">{{ workout.name }}</li>
-      </ul>
-    </div>
+  <div v-for="w in userWorkouts">
+    <h1>Sample Templates</h1>
+    <MiniWorkout :workout="w"/>
   </div>
+  <!-- <div>
+    <v-row>
+      <v-col>
+        <v-sheet height="200">
+          <v-calendar class="h-10 w-20" color="primary"></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </div> -->
 </template>
 
 <script setup lang="ts">
-import displayTemplate from '../components/DisplayTemplate.vue'
+// import displayTemplate from '@/components/DisplayTemplate.vue'
+import type { Ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import type { Workout } from '@/types/workout'
+import MiniWorkout from '@/components/MiniWorkoutView.vue'
 import { VCalendar } from 'vuetify/labs/VCalendar'
+import { getUserWorkouts } from '@/requests/workout'
+
+const contentLoaded = ref(false)
+const userWorkouts:Ref<Workout[]> = ref([])
+
+onMounted(async() => {
+  try {
+    userWorkouts.value = await getUserWorkouts()
+    contentLoaded.value = true
+  } catch(error) {
+    console.error(error)
+  }
+})
+
 </script>
 
 <style scoped></style>
