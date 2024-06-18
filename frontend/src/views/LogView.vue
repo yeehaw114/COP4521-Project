d38f2214050caff441ef0f5355420166c44e51a5
Alex Gonzalez <galex2003@icloud.com>
2024-06-18 11:56:07 -0400

    added ability to view workout logs

--- frontend/src/views/LogView.vue
+++ frontend/src/views/LogView.vue
@@ -1,3 +1,45 @@
-<template></template>
+<template v-if="contentLoaded">
+    <ViewLog :goalWorkout="goalWorkout" :workout="workout" />
+    <Error v-if="errorOccured"/>
+  </template>
+  
+  <script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import { useRoute } from 'vue-router'
+  import type { Workout } from '@/types/workout'
+  import type { Ref } from 'vue'
+  import { getWorkout, stringArrayToInt } from '@/requests/workout'
+  import ViewLog from '@/components/ViewLog.vue'
+  import { getLog } from '@/requests/log'
+  import ErrorComponent from '@/components/ErrorComponent.vue'
+  
+  const contentLoaded = ref(false)
+  const errorOccured = ref(false)
+  const workout: Ref<Workout> = ref({
+    name: '',
+    id: 0,
+    sets: []
+  })
 
-<script setup lang="ts"></script>
+  const goalWorkout: Ref<Workout> = ref({
+    name: '',
+    id: 0,
+    sets: []
+  })
+  
+  onMounted(async () => {
+    try {
+        const workoutid = stringArrayToInt(useRoute().params.workoutid as string[])
+        workout.value = await getWorkout(workoutid)
+
+        const logid = stringArrayToInt(useRoute().params.logid as string[])
+        goalWorkout.value = await getLog(logid)
+        
+        contentLoaded.value = true
+    } catch(error) {
+        errorOccured.value =  true
+        console.error(error)
+    }
+  })
+  </script>
+  
