1527744874dd18493000031b79ceeb5ab895947d
Fernando-deTorres <fdetorressalichs@fsu.edu.com>
2024-06-19 15:05:42 -0400

    About to try to recreate in main

--- backend/backend/middleware/middleware.py
+++ backend/backend/middleware/middleware.py
@@ -1,12 +1,13 @@
-from django.db import connections
 from django.utils.deprecation import MiddlewareMixin
+from django.db import connections
 from django.conf import settings
 
 class RoleBasedDatabaseMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.user.is_authenticated:
-            role = request.user.role.name  # Assuming user has a role attribute
-            if role in settings.DATABASE_ROLES:
+            role = getattr(request.user, 'role', None)  # Assuming role is a CharField
+            if role and role in settings.DATABASE_ROLES:
                 db_role = settings.DATABASE_ROLES[role]
-                connections['default'].settings_dict['USER'] = db_role['USER']
-                connections['default'].settings_dict['PASSWORD'] = db_role['PASSWORD']
+                connection = connections['default']
+                connection.settings_dict['USER'] = db_role['USER']
+                connection.settings_dict['PASSWORD'] = db_role['PASSWORD']
