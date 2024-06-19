2a8e966dfe0560ed2933d08994d5e06ba606ee9e
zlima410 <endercraft68@gmail.com>
2024-06-19 19:08:51 -0400

    Add route for viewing every workout ever

--- backend/workouts/views.py
+++ backend/workouts/views.py
@@ -238,3 +238,9 @@ class WorkoutsViewSet(viewsets.ModelViewSet):
         workouts = Workouts.objects.filter(username=user)
         serializer = WorkoutsSerializer(workouts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(detail=False, methods=['get'], url_path='all-templates')
+    def all_templates(self, request):
+        workouts = Workouts.objects.all()
+        serializer = WorkoutsSerializer(workouts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
