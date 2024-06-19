7e27a3f960b7e6c93b175e1f5593cafef62e15fc
Alex Gonzalez <galex2003@icloud.com>
2024-06-18 23:17:16 -0400

    Updated about view

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,16 @@
 <template>
-  <v-card class="mx-auto pa-6" elevation="15" max-width="500" rounded="md">
+  <v-card class="mx-auto pa-6" elevation="15" max-width="800" rounded="md">
     <div>
       <div align="center" class="mx-auto text-h3">About Us</div>
-      <v-divider :thickness="5"></v-divider>
-      <div class="text-h6">
-        Contributors: Alex Gonzalez, Zachary Lima, Gavin McDavitt, Ethan Burke, Zachary Herman,
-        Fernando de Torres
-      </div>
-      <div class="text-body-1">
-        This is a project made for COP 4521 at Florida State University.
-      </div>
+      <v-card class="mx-auto pa-6" max-width="750" rounded="md">
+        <div class="text-h6">
+          Contributors: Alex Gonzalez, Zachary Lima, Gavin McDavitt, Ethan Burke, Zachary Herman,
+          Fernando de Torres
+        </div>
+        <div class="text-body-1">
+          This is a project made for COP 4521 at Florida State University. We used Vue.js with Vuetify components for the frontend. We used Django for the backend along with a PostgreSQL database. Additionally, we containerized the entire application using Docker.
+        </div>
+      </v-card>
     </div>
   </v-card>
 </template>
